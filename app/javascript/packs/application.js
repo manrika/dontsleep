b5a5a4ca39b206da30432b767af592303cb35c19
manrika malka <manrika.m@gmail.com>
2021-12-31 15:11:20 +0000

    testing hamburger animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,10 @@ window.onload = () => {
     columnWidth: '.grid-sizer'
   });
 };
+
+// Hamburger icon animation
+$(document).ready(function () {
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+    $(this).toggleClass('open');
+  });
+});
