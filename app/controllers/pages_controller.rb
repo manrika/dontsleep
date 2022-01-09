3987b8bfa380220e77f6f7f2ab52aa7c3c1fea56
manrika malka <manrika.malka@fibr.com>
2022-01-09 16:40:27 +0000

    remove new artists, show all artists on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :about, :news]
+  before_action :all_artists, only: [ :home, :editor_home ]
 
   def home
-    @new_artists = Artist.last(4).sort_by(&:id).reverse
     @colors = ["56, 176, 208, 1", "151, 124, 186, 1", "242, 86, 101, 0.79", "112, 198, 171, 1", "246, 192, 48, 1"]
   end
 
@@ -13,7 +13,12 @@ class PagesController < ApplicationController
   end
 
   def editor_home
-    @artists = Artist.all.sort_by(&:id).reverse
     @releases = Release.all.sort_by(&:id).reverse
+  end
+
+  private
+
+  def all_artists
+    @artists = Artist.all.sort_by(&:id).reverse
   end
 end
